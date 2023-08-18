52603a39fc9bdb729273e0fedeb30ac4231ba8f6
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-08-18 16:27:44 +1000

    got react-water-wave working

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import WaterWave from 'react-water-wave';
+import test from './assets/test.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <WaterWave
+      imageUrl={test}
+      style={{ width: '100%', height: '100%', backgroundSize: 'contain' }}
+    >
+      {({ pause, play }) => (
+        <div className="container">
+          <h1>React Water Wave</h1>
+          <h3>
+            MIT ©{' '}
+            <a
+              href="https://github.com/homerchen19"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              homerchen19
+            </a>
+          </h3>
+        </div>
+      )}
+    </WaterWave>
+  );
 }
 
 export default App
