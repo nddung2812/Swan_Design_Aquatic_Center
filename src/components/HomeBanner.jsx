f6150a8e27fa3ada7de7f25eb6ca34fb0023bb84
Johnny Nguyen <56814739+nddung2812@users.noreply.github.com>
2023-08-28 23:50:25 +1000

    add color

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -4,22 +4,24 @@ const HomeBanner = () => {
   return (
     <div className='homebanner-ctn'>
       <h1>Aquatic Plants and Fish</h1>
-      <h2>Bringing the Ocean to You</h2>
+      <h2>Bringing the ocean to you by Aquatic Swan Design</h2>
       <p>Your Source for Healthy Aquarium Fish and Lush Plants</p>
-      <div className="checkout">
-        <a href="">Who we are</a>
-      </div>
+      <a href="">
+        <div className="homebanner-main-cta-btn">
+          Who we are
+        </div>
+      </a>
       <div className="homebanner-cta-ctn">
         <div className="cta-card">
-          <h2>Bucephandraa</h2>
+          <h2>Bucephalandra</h2>
           <p>Discover all products</p>
         </div>
         <div className="cta-card">
-          <h2>Bucephandraa</h2>
+          <h2>D.I.Y Projects</h2>
           <p>Discover all products</p>
         </div>
         <div className="cta-card">
-          <h2>Bucephandraa</h2>
+          <h2>Probiotics</h2>
           <p>Discover all products</p>
         </div>
       </div>
