ac8ad11838ddbba3b0e2b644861347fb816928e9
Dung Nguyen <nddung2812@gmail.com>
2023-08-30 14:36:21 +1000

    add menu toggle

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -15,13 +15,18 @@ const HamburgerMenu = () => {
           <div className="line"></div>
         </div>
       ) : (
-        <>
-          <div className="navbar-item">Home</div>
-          <div className="navbar-item">Blogs</div>
-          <div className="navbar-item">D.I.Y Tips</div>
-          <div className="navbar-item">Exhibition</div>
-          <div className="navbar-item">Contact</div>
-        </>
+        <div className='navbar-mobile-wrapper'>
+          <div>
+            <div className="navbar-item">Home</div>
+            <div className="navbar-item">Blogs</div>
+            <div className="navbar-item">D.I.Y Tips</div>
+            <div className="navbar-item">Exhibition</div>
+            <div className="navbar-item">Contact</div>
+          </div>
+          <div>
+            <div className='navbar-close-btn' onClick={handleMenuClick}>&#10005;</div>
+          </div>
+        </div>
       )}
     </>
   );
