4d03fed66c9878b5f588ab330538cf5883f0a505
unknown <nddung2812@gmail.com>
2024-04-07 13:06:38 +1000

    add GTM

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OvO5l4q+fjOTmCFiQKRTOz41GKKPYJFc0ceJPIH6jc4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ED/WQXwgrB9nOnRXkO2jBFLNqeCKWP/AGvr/FMA1W20=\"\n}"
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Duckaroo | Aquatic Swan Design",
-  description: "Australia Wide Shipping Aquatic Rare & Rarer Plants • Live Arrival Guarantee • 100% Customer Satisfaction Guarantee. Aquatic Swan Design ! Unique Brisbane aquarium online rare plants. We can help you with maintenance, custom aquariums, plants, fish and more!",
+  title: "Aquatic Swan Design | Duckaroo | Tank Service",
+  description: "Australia Wide Shipping Aquatic Rare & Rarer Plants • Live Arrival Guarantee • 100% Customer Satisfaction Guarantee. Aquatic Swan Design! Unique Brisbane aquarium online rare plants. We can help you with maintenance, custom aquariums, plants, fish and more!",
   icons: {
     icon: "/swan-favicon.png",
   },
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GoogleTagManager gtmId='GTM-NQQBXXPZ'/>
         {children}
       </body>
     </html>
