9729609227f63c54429d7a72d1f0804cb25264e7
unknown <nddung2812@gmail.com>
2024-04-14 10:13:15 +1000

    update Metadata

--- app/components/HomeBanner.jsx
+++ app/components/HomeBanner.jsx
@@ -1,48 +1,62 @@
-import '../styles/Homebanner.css'
+import "../styles/Homebanner.css";
 import { TfiMusic, TfiControlPause } from "react-icons/tfi";
-import Link from 'next/link'
+import Link from "next/link";
 
-const HomeBanner = ({music, setMusic}) => {
+const HomeBanner = ({ music, setMusic }) => {
   const handleMusic = () => {
-    console.log(music)
-    setMusic(!music)
-  }
+    setMusic(!music);
+  };
   return (
-    <main className='homebanner-ctn'>
+    <main className="homebanner-ctn">
       <h1>Aquatic Plants and Fish</h1>
       <h2>Bringing the ocean to you by Aquatic Swan Design</h2>
       <p>Your Source for Healthy Aquarium Fish and Lush Plants</p>
-      <Link href="https://duckaroo.com.au/pages/about-us" className="homebanner-main-cta-btn">
-          About Us
+      <Link
+        href="https://duckaroo.com.au/pages/about-us"
+        className="homebanner-main-cta-btn"
+      >
+        About Us
       </Link>
       <div className="homebanner-cta-ctn">
-        <a target="_blank" href='https://duckaroo.com.au/collections/aquarium-plants' rel="noreferrer">
+        <a
+          target="_blank"
+          href="https://duckaroo.com.au/collections/aquarium-plants"
+          rel="noreferrer"
+        >
           <div className="cta-card cta-btn1">
             <h2>Bucephalandra</h2>
             <p>Discover rare variants</p>
           </div>
         </a>
-        <a target="_blank" href="https://duckaroo.com.au/pages/our-services" rel="noreferrer">
+        <a
+          target="_blank"
+          href="https://duckaroo.com.au/pages/our-services"
+          rel="noreferrer"
+        >
           <div className="cta-card cta-btn2">
             <h2>D.I.Y Projects</h2>
             <p>Checkout top-quality items</p>
           </div>
         </a>
-        <a target="_blank" href="https://duckaroo.com.au/collections/aquarium-probiotics" rel="noreferrer">
+        <a
+          target="_blank"
+          href="https://duckaroo.com.au/collections/aquarium-probiotics"
+          rel="noreferrer"
+        >
           <div className="cta-card cta-btn3">
             <h2>Probiotics</h2>
             <p>Powered by Koika</p>
           </div>
         </a>
       </div>
-      <div className='music-controal-ctn'>
-        {music ? <div className='music-btn' onClick={handleMusic}><TfiMusic color="#fff" />
-        </div> :
-        <div className='music-btn' onClick={handleMusic}><TfiControlPause color="#fff" /></div>}
+      <div className="music-controal-ctn">
+        <div className="music-btn" onClick={handleMusic}>
+          {music ? <TfiMusic color="#fff" /> : <TfiControlPause color="#fff" />}
+        </div>
         <p>Sound</p>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,26 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Aquatic Swan Design | Duckaroo | Tank Service",
-  description: "Australia Wide Shipping Aquatic Rare & Rarer Plants • Live Arrival Guarantee • 100% Customer Satisfaction Guarantee. Aquatic Swan Design! Unique Brisbane aquarium online rare plants. We can help you with maintenance, custom aquariums, plants, fish and more!",
+  description:
+    "Discover Bucephranda's premier tank services, featuring Australia Wide Shipping for Aquatic Rare & Rarer Plants. Enjoy our Live Arrival Guarantee and 100% Customer Satisfaction Guarantee. Explore our exclusive Aquatic Swan Design selection, offering unique Brisbane aquarium online rare plants. Let us assist you with maintenance, custom aquariums, plants, fish, and more!",
   icons: {
     icon: "/swan-favicon.png",
   },
-  image: "https://firebasestorage.googleapis.com/v0/b/aquatic-swan-design.appspot.com/o/oceanBlue.jfif?alt=media&token=578048fa-cd06-4601-9ce5-9763001a79b9"
+  metadataBase: new URL(
+    "https://firebasestorage.googleapis.com/v0/b/aquatic-swan-design.appspot.com/o/oceanBlue.jfif?alt=media&token=578048fa-cd06-4601-9ce5-9763001a79b9"
+  ),
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <GoogleTagManager gtmId='GTM-NQQBXXPZ'/>
+        <GoogleTagManager gtmId="GTM-NQQBXXPZ" />
         <GoogleAnalytics gaId="G-DMVQ6Y0D0S" />
         {children}
       </body>
