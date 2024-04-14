f5a6608c1e082c66bb39eb527e1820bac58d876f
unknown <nddung2812@gmail.com>
2024-04-14 21:47:55 +1000

    update to 14.2.1

--- app/service/page.js
+++ app/service/page.js
@@ -1,23 +1,28 @@
-import dynamic from 'next/dynamic'
-import Footer from '../components/Footer';
-import Image from 'next/image'
-import './service.css'
+import dynamic from "next/dynamic";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import "./service.css";
 
-const NavbarWithNoSSR = dynamic(
-    () => import('../components/Navbar'),
-    { ssr: false }
-  )
+const NavbarWithNoSSR = dynamic(() => import("../components/Navbar"), {
+  ssr: false,
+});
 
 export default function Service() {
-    return (
-        <div className='service-ctn' >
-        <NavbarWithNoSSR />
-        <main>
-            Service
-            <Image src="https://firebasestorage.googleapis.com/v0/b/aquatic-swan-design.appspot.com/o/oceanBlue.jfif?alt=media&token=578048fa-cd06-4601-9ce5-9763001a79b9" width={500} height={500} alt="ocean" />
-        </main>
-        <Footer />
+  return (
+    <>
+      <main>
+        <div className="service-ctn">
+          <NavbarWithNoSSR />
+          Service
+          <Image
+            src="https://firebasestorage.googleapis.com/v0/b/aquatic-swan-design.appspot.com/o/oceanBlue.jfif?alt=media&token=578048fa-cd06-4601-9ce5-9763001a79b9"
+            width={500}
+            height={500}
+            alt="ocean"
+          />
+          <Footer />
         </div>
-    );
-  }
-  
+      </main>
+    </>
+  );
+}
