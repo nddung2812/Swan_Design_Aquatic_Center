ceda12d52d402fb046d235d8b8ae46f5cd7fc4b0
Dung Nguyen <nddung2812@gmail.com>
2024-05-04 23:50:47 +1000

    test form

--- app/service/serviceComponents/banner.js
+++ app/service/serviceComponents/banner.js
@@ -1,17 +1,19 @@
 import Image from "next/image.js";
 import styles from "./ServiceBanner.module.scss";
+import ServiceForm from "./serviceForm.js";
 const ServiceBanner = () => {
   return (
     <div className={styles.BannerCtn}>
       <div className={styles.BannerContentContainer}>
         <div className={styles.ServiceContent}>
-          <h1>Expert Fish Tank & Pond Cleaning Maintenance Services</h1>
+          <h1> Fish Tank & Pond Cleaning Services in Brisbane Areas</h1>
           <p>
             Whether you require aquarium or pond cleaning, maintenance, new
             setups, equipment installations, modifications, or aquarium
-            relocation of any size, Aquatic Swan Design is here to assist you!
+            relocation of any size around Brisbane, Duckaroo is here to assist you!
           </p>
         </div>
+      <ServiceForm />
       </div>
     </div>
   );
