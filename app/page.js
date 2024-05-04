8bc9d712902bb8b3dbea141046a82b45cd4ae708
Dung Nguyen <nddung2812@gmail.com>
2024-05-04 22:44:50 +1000

    add back music and video

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import Footer from "./components/Footer";
 import HomeBanner from "./components/HomeBanner";
 import { Preloader } from "./components/Preloader.jsx";
 import dynamic from "next/dynamic";
-
 const NavbarWithNoSSR = dynamic(() => import("./components/Navbar"), {
   ssr: false,
 });
@@ -18,6 +17,10 @@ const WaterWaveNoSSr = dynamic(() => import("react-water-wave"), {
 
 const Home = () => {
   const [music, setMusic] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const handleLoadedData = () => {
+    setIsLoading(false);
+  };
   return (
     <>
       <Preloader />
@@ -29,6 +32,10 @@ const Home = () => {
       >
         {() => (
           <main className="home-page-container">
+                {isLoading && <div className="corp-video-loader-ctn">
+            <div className="corp-video-loader"></div>
+            <div className="corp-video-textloader">Loading...</div>
+          </div>}
             <video
               autoPlay="autoplay"
               muted
@@ -36,15 +43,17 @@ const Home = () => {
               playsInline
               loop="loop"
               preload="auto"
+              onLoadedData={handleLoadedData}
+              style={{ display: isLoading ? "none" : "block" }}
               stop
             >
               <source
-                src="https://znjf1ip6migqhqsx.public.blob.vercel-storage.com/Koifish-yTANtUJgMxz90jG7i5JvODHDj2RVQO.mp4"
+                src='https://firebasestorage.googleapis.com/v0/b/aquatic-swan-design.appspot.com/o/tinywow_Koifish_52130439.mp4?alt=media&token=8c1f6c99-1e67-4241-9f83-04e724c932d7'
                 type="video/mp4"
               />
             </video>
             <ReactHowler
-              src="https://znjf1ip6migqhqsx.public.blob.vercel-storage.com/BlueDream-3zIGmjQ2jnpCR09dSO0k2olRfYnEEO.mp3"
+              src="https://firebasestorage.googleapis.com/v0/b/aquatic-swan-design.appspot.com/o/Blue%20Dream%20-%20Cheel.mp3?alt=media&token=2cc7a283-16a3-4494-a9fe-6e79e867ed81"
               playing={music}
               volume={0.4}
               loop={true}
